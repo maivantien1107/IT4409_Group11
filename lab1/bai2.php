f57827219e651900c4b44284c11eeaeccc75592b
maivantien1107 <maivantien1107@users.noreply.github.com>
2022-04-12 22:46:28 +0700

    add lab1

---
+++ lab1/bai2.php
@@ -0,0 +1,76 @@
+<html>
+ <body>
+     <style>
+         table, th, tr, td {  
+            border: 1px solid black;
+            padding-left: 5px;
+            font-size:18px;
+         }
+        
+         table {
+     border-collapse: collapse;
+}
+        th {
+            height:200px;
+        }
+        .heading1{
+            text-align:left;
+            width: 250px;
+            vertical-align: top;
+        }
+        .heading2{
+            width: 250px;
+            text-align: center;
+            vertical-align: top;
+        }
+        
+        
+        tr:nth-child(even) {background-color: #f2f2f2;}
+        
+     </style>
+     <table>
+         <thead>
+         <tr>
+             <th class="heading1" colspan="2">Personal Details</th>
+             
+             <th class="heading2">Image</th>
+         </tr>
+         </thead>
+    <tbody>
+         <tr class="detail">
+             <td class="title1">Education process</td>
+             
+             <td colspan="2" style="width:500px;"></td>
+         </tr>
+         <tr class="detail">
+             <td class="title1" >Working process</td>
+             <td colspan="2" style="width:500px;"></td>
+         </tr>
+         <tr class="detail">
+             <td class="title1">Society activities</td>
+             <td colspan="2" style="width:500px;"></td>
+         </tr>
+         <tr class="detail">
+             <td class="title1">Skills</td>
+             <td colspan="2" style="width:500px;"></td>
+         </tr>
+         <tr class="detail">
+             <td class="title1">Experiences</td>
+             <td colspan="2" style="width:500px;"></td>
+         </tr>
+         <tr class="detail">
+             <td class="title1">Certifications</td>
+             <td colspan="2" style="width:500px;"></td>
+         </tr>
+         <tr class="detail">
+             <td class="title1">Hobbies</td>
+             <td colspan="2" style="width:500px;"></td>
+         </tr>
+         <tr class="detail">
+             <td class="title1">Desire</td>
+             <td colspan="2" style="width:500px;"></td>
+         </tr>
+    </tbody>
+     </table>
+ </body>   
+</html>
